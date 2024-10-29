b6957e4ea5cc27226ac13accd85a7097b13c7c7c
sudarshanbadu <darshanbadug@gmail.com>
2024-10-29 13:33:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# printer-excercise
+ 
---
+++ printer excercise/src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       /*  System.out.println("Hello, World!");
+        System.out.println("My name is Naran");
+        */
+        String creator = "Narayan";
+        System.out.println("The app is made by "+ creator);
+        int number1 = 85;
+        int number2 = 67;
+       // System.out.println("The value of number1 is " + number1);
+        number1 = 5;
+        System.out.println("The value of number1 is " + number1);
+        number2 = 3;
+        System.out.println("The value of number2 is " + number2);
+        int multiplication;
+        multiplication = number1* number2;
+        System.out.println(number1 +" * " +number2 +" =" + multiplication);
+        int add = number1 + number2;
+        System.out.println(number1 +" + " +number2 +" =" + add);
+        int sub =  number1 - number2;
+        System.out.println(number1 +" - " +number2 +" =" + sub);
+        int div =  number1 % number2;
+        System.out.println(number1 +" % " +number2 +" =" + div);
+  }
+}
